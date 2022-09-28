bb62803b42b0aa84905c47ba1d09cd79a57fd627
mitayRflux <daflux2@gmail.com>
2022-09-28 23:27:11 +0300

    sorting

--- js/bg-elements.js
+++ js/bg-elements.js
@@ -1,10 +1,9 @@
 const bgElements = ()=>{
-  const elements = document.querySelectorAll('.set-bg');
-for(let i= 0;i<elements.length;i++){
-const src = elements[i].dataset.setbg;
-//elements[i].style.backgroundImage= 'url('+src+')';
-elements[i].style.backgroundImage= `url(${src})`;
-}
-}
+const elements = document.querySelectorAll('.set-bg');
+elements.forEach((elem)=>{
+  elem.style.backgroundImage= `url(${elem.dataset.setbg})`;
+});
+
+};
 
 bgElements();
--- js/mainData.js
+++ js/mainData.js
@@ -1,10 +1,39 @@
 const mainData = ()=>{
+   const renderAnimeList = (arrey,ganres)=>{
+     console.log(arrey);
+     console.log(ganres);
+
+   };
+
+  const renderTopAnime = (arrey)=>{
+    const wrapp = document.querySelector('.filter__gallery');
+    wrapp.innerHTML='';
+      arrey.forEach((item)=>{
+    wrapp.insertAdjacentHTML('afterBegin',`
+    <div class="product__sidebar__view__item set-bg mix"
+       data-setbg="${item.image}">
+        <div class="ep">${item.rating} / 10</div>
+         <div class="view"><i class="fa fa-eye"></i>${item.views}</div>
+          <h5><a href="/anime-details.html">${item.title}</a></h5>
+    </div>
+    `);
+    });
+    wrapp.querySelectorAll('.set-bg').forEach((elem)=>{
+      elem.style.backgroundImage= `url(${elem.dataset.setbg})`;
+    });
+  };
+
 fetch('./db.json')
 .then((response)=>{
   return response.json();
 })
   .then((data)=>{
-    console.log(data.anime[35]);
+    const ganres=new Set();
+    renderTopAnime(data.anime.sort((a,b)=> b.views-a.views).slice(0,5));
+    data.anime.forEach((item)=>{
+      ganres.add(item.ganre);
+    });
+    renderAnimeList(data.anime,ganres);
   });
 };
 
